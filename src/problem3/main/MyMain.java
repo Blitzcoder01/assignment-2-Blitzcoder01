f7874fc4d3f0ba17ff73273a80faf13ec91639ff
riya <saxenaritu1979@gmail.com>
2020-03-28 01:07:59 +0530

    creation of MyMain class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -11,12 +11,6 @@ import problem3.myqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
-    public  static void main(String[] args){
-        MyPriorityQueue obj = new MyPriorityQueue();
-        obj.enqueue("sara",16);
-        obj.enqueue("riya",27);
-        obj.enqueue("apala", 23);
-        obj.view();
-    }
+
 
 }
