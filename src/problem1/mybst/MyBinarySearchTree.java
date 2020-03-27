a0f1e35832e94ada96557ed52ffeb75181169719
riya <saxenaritu1979@gmail.com>
2020-03-28 00:01:38 +0530

    completion of insert function

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -20,6 +20,43 @@ public class MyBinarySearchTree {
     public void insert(int data){
         TreeNode ns = new TreeNode();
         ns.setdata(data);
+        if(root == null){
+            root = ns;
+        }
+        else{
+            TreeNode current = root;
+            while(true){
+                if(data > current.getdata()){
+                    if(current.getRight()==null){
+                        current.setRight(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getRight();
+                    }
+                }
+                else{
+                    if(current.getLeft()==null){
+                        current.setLeft(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getLeft();
+                    }
+                }
+            }
+        }
     }
+    private void LeftNode(TreeNode root){
+        if(root == null)    return;
+        else{
+            if(root.getLeft()!=null) System.out.println(root.getLeft().getdata());
+            else    count++;
+            LeftNode(root.getLeft());
+            LeftNode(root.getRight());
+        }
+
+
+}
 
 }
