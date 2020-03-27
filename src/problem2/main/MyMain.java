c8b5013fe5046d96db2f8a034843cd2fa7213825
riya <saxenaritu1979@gmail.com>
2020-03-28 00:42:50 +0530

    calling of insert function

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -13,5 +13,14 @@ import problem1.mybst.MyBinarySearchTree;
 public class MyMain {
     public static void main(String [] args){
         MyBinarySearchTree tree = new MyBinarySearchTree();
+        System.out.println("Pre order and post order traversal of Binary Search Tree :");
+        tree.insert(17);
+        tree.insert(12);
+        tree.insert(11);
+        tree.insert(26);
+        tree.insert(53);
+        tree.insert(40);
+        tree.insert(31);
+
     }
 }
