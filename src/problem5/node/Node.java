47d862eea73a14fa3d666d279340b5f67f5c3ab9
riya <saxenaritu1979@gmail.com>
2020-03-28 09:04:23 +0530

    Node class creation

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -10,22 +10,5 @@ import problem5.student.Student;
 
 // to define node properties
 public class Node {
-    Student student;
-    Node next;
 
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
-
-    public Student getStudent() {
-        return student;
-    }
-
-    public Node getNext() {
-        return next;
-    }
 }
