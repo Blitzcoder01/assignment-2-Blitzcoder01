874ff909f25dd413872b3735a641f359659a472e
riya <saxenaritu1979@gmail.com>
2020-03-28 08:36:54 +0530

    enqueue() function creation

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -38,6 +38,19 @@ public class MyQueue {
         front = null;
         rear = null;
     }
+
+    public void enqueue(int data){
+        Node ns = new Node();
+        ns.setData(data);
+        if(front==null){
+            front = ns;
+            rear = ns;
+        }
+        else{
+            rear.setNext(ns);
+            rear = ns;
+        }
+    }
 }
 
 
