41610d5f5acb5e429703b73b82ceb82aa6d48ca5
riya <saxenaritu1979@gmail.com>
2020-03-28 09:05:35 +0530

    variable creation and getters and setters creation

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -10,5 +10,22 @@ import problem5.student.Student;
 
 // to define node properties
 public class Node {
+    Student student;
+    Node next;
 
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
