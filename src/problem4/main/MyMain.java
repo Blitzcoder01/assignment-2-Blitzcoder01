1e3f1535b7a8d8374e8a4a0a58ddca978864e0df
riya <saxenaritu1979@gmail.com>
2020-03-28 08:26:29 +0530

    MyMain class creation

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -13,19 +13,5 @@ import problem4.myqueue.MyQueue;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
-    public static void main(String args[]) {
-        MyBinarySearchTree obj = new MyBinarySearchTree();
-        System.out.println("queue to store pre-order successor of all the nodes of given binary search tree is : ");
-        obj.insert(12);
-        obj.insert(31);
-        obj.insert(32);
-        obj.insert(14);
-        obj.insert(23);
-        obj.insert(10);
-        obj.insert(17);
 
-        MyQueue mq = new MyQueue();
-        mq.preSuccessor(obj.getroot());
-        mq.getPreSucc();
-    }
 }
